daeb04c6c11c0ed6ba8f90597f7203f240826e62
Walter Shewmake <25shewmwalj@washk12.org>
2018-11-21 13:21:23 -0700

    xzcfazx

--- main.py
+++ main.py
@@ -1,1 +1,43 @@
-import string
+q = input('0 = ENCRYPT, 1 = DECRYPT >> ')
+if (q == '0'):
+    text = input('Input >> ')
+    textFinal = ''
+
+    # makes text into string
+
+    text = str(text)
+
+    # seperates text by char into an array
+
+    text = list(text)
+
+    # changes to numbers
+    for i in range (len(text)):
+        text[i] = ord(text[i])
+        print(text)
+
+    # converts numbers
+
+    for i in range (len(text)):
+        text[i] = text[i] + 1;
+        if (text[i] > 126):
+            text[i] = text[i] - 126 + 31
+        print(text)
+        
+    ## change back to string
+
+    for i in range (len(text)):
+        text[i] = chr(text[i])
+         
+    # prints text
+
+    print(text)
+
+    textFinal = ",".join(text)
+    textFinal = textFinal.replace(",","")
+
+    # prints text
+
+    print (textFinal)
+else:
+    print('hi')
