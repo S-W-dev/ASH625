0dcd291f8457875d272d823021d9f45176933176
WhiteFire356 <waltershewmake@gmail.com>
2018-11-23 15:31:05 -0700

    sd

--- main.py
+++ main.py
@@ -40,4 +40,42 @@ if (q == '0'):
 
     print (textFinal)
 else:
-    print('hi')
+    text = input('Input >> ')
+    textFinal = ''
+
+    # makes text into string
+
+    text = str(text)
+
+    # seperates text by char into an array
+
+    text = list(text)
+
+    # changes to numbers
+    for i in range (len(text)):
+        text[i] = ord(text[i])
+        print(text)
+
+    # converts numbers
+
+    for i in range (len(text)):
+        text[i] = text[i] + 1;
+        if (text[i] > 126):
+            text[i] = text[i] - 126 + 31
+        print(text)
+        
+    ## change back to string
+
+    for i in range (len(text)):
+        text[i] = chr(text[i])
+         
+    # prints text
+
+    print(text)
+
+    textFinal = ",".join(text)
+    textFinal = textFinal.replace(",","")
+
+    # prints text
+
+    print (textFinal)
